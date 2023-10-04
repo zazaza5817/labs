aff506230ab50b195a92dd0b4383dd770aeecaf2
Nikita <49568244+zazaza5817@users.noreply.github.com>
2023-10-04 14:03:05 +0300

    Update mymain.py

--- lab4/mymain.py
+++ lab4/mymain.py
@@ -1,53 +1,44 @@
-from math import sin, log
+# Неделин Никита ИУ7-16Б
+# Программа для расчета таблицы значений функций и вывода графика одной из них
+from math import log  # Подключение библиотеки
 
-
-#  definition of my functions
-def a_generator(start, stop, step):
-    while start <= stop:
-        yield start
-        start += step
-
-
-def compare_by_eps(a, b):
-    eps = 1e-2
-    return abs(a - b) < eps
-
-
-def h2(x):
-    y = sin(x)
-    return y
-
-
-def h1(x):
-    y = x / 2
-    return y
-
-
-a = -1
-b = 2
-step = 0.2
+a = 16                     # Число с которого следует перебирать иксы
+b = 20                     # Число до которого следует перебирать иксы
+step = 0.1                 # Шаг с которым следует перебирать иксы
+range_coefficient = 10     # Коэффициент для вычисления дробных значений используя range()
+graph_length = 120         # Длина графика
 
 # Вывод шапки таблицы
 print("-" * 46)
 print(f'| {"a":^12} | {"h1":^12} | {"h2":^12} |')
 print("-" * 46)
 
-values_count = 0
-for i in range(a * 10, int((b + step) * 10), int(step * 10)):
-    print(f'| {i:^12.5g} | {h1(i):^12.5g} | {h2(i):^12.5g} |')
-    values_count += 1
+# Вывод самой таблицы и подсчет минимума и максимума выводимой в график функции
+min_value = float('+inf')
+max_value = float('-inf')
+for i in range(int(a * range_coefficient), int((b + step) * range_coefficient), int(step * range_coefficient)):
+    i /= range_coefficient
+    h1 = i**3 - 5.57*(i**2) - 193 * i - 633.1
+    h2 = i * log(i) - 52
+    # h2 = 1
+    max_value = max(max_value, h2)
+    min_value = min(min_value, h2)
+    print(f'| {i:^12.5g} | {h1:^12.5g} | {h2:^12.5g} |')
 print("-" * 46)
 
-# serif_count = int(input())  # input serif count
-serif_count = 4
+# ввод кол-ва засечек
+try:
+    serif_count = int(input("Введите кол-во засечек: "))
+except ValueError:
+    raise ValueError("Кол-во засечек должно быть целым")
+if not(4 <= serif_count <= 8):
+    raise ValueError('Количество засечек должно быть от 4 до 8')
 
 # Расчет значений на засечках графика
-h2_values = list(map(h2, a_generator(a, b, step)))
-graph_length = 120
-delta = (max(h2_values) - min(h2_values)) / (serif_count - 1)
+delta = (max_value - min_value) / (serif_count - 1)
 serif_values = []
 for i in range(serif_count):
-    serif_values.append(min(h2_values) + delta * i)
+    serif_values.append(min_value + delta * i)
 
 # Вывод шапки графика
 title = "Построение графика"
@@ -60,34 +51,30 @@ print("|")
 
 # Подсчет коэффициента для вывода графика
 try:
-    scale_coefficient = (graph_length - 1) / (max(h2_values) - min(h2_values))
-except:
-    raise ValueError("Невозможно построить график, содержащий одну точку")
+    scale_coefficient = (graph_length - 1) / (max_value - min_value)
+    # print(scale_coefficient)
+except ZeroDivisionError:
+    scale_coefficient = (graph_length - 1) / (max_value / 2)
 
-# Нахождение точки 0 на графике
-space_position = round(0 - min(h2_values) * scale_coefficient) + 1
-
+# Нахождение y = 0 на графике
+space_position = round(0 - min_value * scale_coefficient) + 1
 
 # Вывод графика
-for x in range(a * 10, int((b + step) * 10), int(step * 10)):
+for x in range(int(a * range_coefficient), int((b + step) * range_coefficient), int(step * range_coefficient)):
     x = x / 10
-    print(f"{x:<10.5g}|", end="")                                     # Вывод оси Х
-    h2 = sin(x)
-    normalized_value = h2 - min(h2_values)                            # Нормализация значения в точке х для вывода
-    dot_position = round(normalized_value * scale_coefficient) + 1    # Положение точки относительно левого края графика
-    spaces_after = (graph_length - dot_position)                      # Количество пробелов после точки графика
-    if 0 < space_position < graph_length:                             # Проверка на пересечение графиком Ох
+    print(f"{x:<10.5g}|", end="")  # Вывод оси Х
+    h2 = x * log(x) - 52  # Нахождение значения функции в точке х
+    normalized_value = h2 - min_value  # Нормализация значения в точке х для вывода
+    dot_position = round(normalized_value * scale_coefficient) + 1  # Положение точки относительно левого края графика
+    spaces_after = (graph_length - dot_position)  # Количество пробелов после точки графика
+    if 0 < space_position < graph_length:  # Проверка на пересечение графиком Ох
         if space_position < dot_position:
             print(" " * (space_position - 1) + "|" + " " * (
-                        dot_position - space_position - 1) + "*" + " " * spaces_after + "|")
+                    dot_position - space_position - 1) + "*" + " " * spaces_after + "|")
         elif space_position > dot_position:
             print(" " * (dot_position - 1) + "*" + " " * (space_position - dot_position - 1) + "|" + " " * (
-                        spaces_after - (space_position - dot_position)) + "|")
+                    spaces_after - (space_position - dot_position)) + "|")
         else:
             print(" " * (dot_position - 1) + "*" + " " * spaces_after + "|")
     else:
         print(" " * (dot_position - 1) + "*" + " " * spaces_after + "|")  # Вывод строчки без пересечения с Ох
-
-# while True:
-#     print('ПУТИН КРУТОЙ!!!!!')
-#     print('ЗЕЛЕНСКИЙ ЛОХ!!!')
