b06586a0c1c1b10792e653a8093aa35d8e2a1211
Nikita <49568244+zazaza5817@users.noreply.github.com>
2023-10-04 21:37:40 +0300

    Update main.py

--- lab4/main.py
+++ lab4/main.py
@@ -10,25 +10,26 @@ if step < 1:
     range_coefficient = int(1/step)+1
 else:
     range_coefficient = 1
-graph_length = 3*20+32                # Длина графика
+graph_length = 3*35+32                # Длина графика
 additional_if_const = 10              # Добавочное значение для графика если Y = const
 
 # Вывод шапки таблицы
-print("-" * 46)
-print(f'| {"a":^12} | {"h1":^12} | {"h2":^12} |')
-print("-" * 46)
+print("-" * 61)
+print(f'| {"r":^12} | {"q1":^12} | {"q2":^12} | {"q3":^12} |')
+print("-" * 61)
 
 # Вывод самой таблицы и подсчет минимума и максимума выводимой в график функции
 min_value = float('+inf')
 max_value = float('-inf')
-for i in range(int(a * range_coefficient), int((b + step) * range_coefficient), int(step * range_coefficient)):
-    i /= range_coefficient
-    h1 = i**3 - 5.57*(i**2) - 193 * i - 633.1
-    h2 = i * log(i) - 52
-    max_value = max(max_value, h2)
-    min_value = min(min_value, h2)
-    print(f'| {i:^12.5g} | {h1:^12.5g} | {h2:^12.5g} |')
-print("-" * 46)
+for r in range(int(a * range_coefficient), int((b + step) * range_coefficient), int(step * range_coefficient)):
+    r /= range_coefficient
+    q1 = r ** 3 - 5.57 * (r ** 2) - 193 * r - 633.1
+    q2 = r * log(r) - 52
+    q3 = (q1 ** 3 - q2 ** 3) / 1000
+    max_value = max(max_value, q3)
+    min_value = min(min_value, q3)
+    print(f'| {r:^12.5g} | {q1:^12.5g} | {q2:^12.5g} | {q3:^12.5g} |')
+print("-" * 61)
 
 # ввод кол-ва засечек
 try:
@@ -40,6 +41,7 @@ if not(4 <= serif_count <= 8):
 
 
 # Вывод шапки графика
+symbols_per_sherif = 10
 if max_value == min_value:
     min_value = min_value - additional_if_const
     max_value = max_value + additional_if_const
@@ -47,14 +49,14 @@ delta = (max_value - min_value) / (serif_count - 1)
 title = "Построение графика"
 print(f"{title:=^{graph_length + 12}s}")  # +12 для отступа под ось х
 # Вычисление кол-ва пробелов между засечками
-spaces_between_serifs = round((graph_length - serif_count * 8) / (serif_count - 1))
+spaces_between_serifs = round((graph_length - serif_count * symbols_per_sherif) / (serif_count - 1))
 print(f"          |", end="")
 for i in range(serif_count):
     serif_value = min_value + delta * i
     if i < serif_count - 1:
-        print(f"{serif_value:<8.5g}" + " " * spaces_between_serifs, end="")
+        print(f"{serif_value:<{symbols_per_sherif}.4g}" + " " * spaces_between_serifs, end="")
     else:
-        print(f"{serif_value:>8.5g}")
+        print(f"{serif_value:>{symbols_per_sherif}.4g}")
 
 # Подсчет коэффициента для вывода графика
 try:
@@ -69,20 +71,22 @@ except ZeroDivisionError:
 space_position = round(0 - min_value * scale_coefficient) + 1
 
 # Вывод графика
-for x in range(int(a * range_coefficient), int((b + step) * range_coefficient), int(step * range_coefficient)):
-    x = x / range_coefficient
-    print(f"{x:<10.5g}|", end="")                                   # Вывод оси Х
-    h2 = x * log(x) - 52                                            # Нахождение значения функции в точке х
-    normalized_value = h2 - min_value                               # Нормализация значения в точке х для вывода
+for r in range(int(a * range_coefficient), int((b + step) * range_coefficient), int(step * range_coefficient)):
+    r = r / range_coefficient
+    print(f"{r:^10.5g}|", end="")                                   # Вывод оси Х
+    q1 = r ** 3 - 5.57 * (r ** 2) - 193 * r - 633.1
+    q2 = r * log(r) - 52                                            # Нахождение значения функции в точке х
+    q3 = (q1 ** 3 - q2 ** 3) / 1000
+    normalized_value = q3 - min_value                               # Нормализация значения в точке х для вывода
     dot_position = round(normalized_value * scale_coefficient) + 1  # Положение точки относительно левого края графика
     spaces_after = (graph_length - dot_position)                    # Количество пробелов после точки графика
     if 0 < space_position < graph_length:                           # Проверка на пересечение графиком Ох
         if space_position < dot_position:
-            print(" " * (space_position - 1) + "|" + " " * (
-                    dot_position - space_position - 1) + "*" + " " * spaces_after + "|")  # Вывод строчки
+            print(" " * (space_position - 1) + "|" + " " * (dot_position - space_position - 1) +
+                  "*" + " " * spaces_after + "|")  # Вывод строчки
         elif space_position > dot_position:
-            print(" " * (dot_position - 1) + "*" + " " * (space_position - dot_position - 1) + "|" + " " * (
-                    spaces_after - (space_position - dot_position)) + "|")  # Вывод строчки
+            print(" " * (dot_position - 1) + "*" + " " * (space_position - dot_position - 1) + "|" +
+                  " " * (spaces_after - (space_position - dot_position)) + "|")  # Вывод строчки
         else:
             print(" " * (dot_position - 1) + "*" + " " * spaces_after + "|")  # Вывод строчки
     else:
