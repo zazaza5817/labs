5f8f726be71347b8924fa37dd7bae295894db930
Nikita <49568244+zazaza5817@users.noreply.github.com>
2023-10-17 17:56:48 +0300

    lab6 update

--- lab6/1.py
+++ lab6/1.py
@@ -5,10 +5,13 @@ print(a)
 
 # 1b
 a = [1, 2, 3, 4, 5, 6, 7, 8]
-index = 3
+index = 2
 obj = "inserted"
-a.append('')
-for i in range(len(a)-1, index, -1):
-    a[i] = a[i-1]
-a[index] = obj
-print(a)
+if 0 <= index <= len(a):
+    a.append('')
+    for i in range(len(a)-1, index, -1):
+        a[i] = a[i-1]
+    a[index] = obj
+    print(a)
+else:
+    print("Индекс не находится в списке")
--- lab6/3.py
+++ lab6/3.py
@@ -1,9 +1,17 @@
 a = [1, 2, 3, 4, 5, 4, 3, 2, 1]
+k = int(input("Введите К: "))
 
 i = 1
+extr = []
 while i < len(a)-1:
     if a[i-1] > a[i] < a[i+1]:
-        print(a[i])
+        extr.append(a[i])
     elif a[i-1] < a[i] > a[i+1]:
-        print(a[i])
+        extr.append(a[i])
     i += 1
+if k > len(extr):
+    print("Не существует экстремума с таким номером")
+elif k < 1:
+    print("Значение номера экстремума должно быть больше нуля")
+else:
+    print(extr[k-1])
